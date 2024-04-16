63779a5fc466745c3c05ea7e1b23882237736119
dvirlabs <dvirlabs@gmail.com>
2024-04-16 04:20:31 +0300

    add title and fix error position

--- frontend/add-targets/src/App.tsx
+++ frontend/add-targets/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
 import InsertTarget from './insertTarget';
+import Title from './title';
 
 function App() {
   return (
     <div className="App">
+      <Title />
       <InsertTarget />
     </div>
   );
